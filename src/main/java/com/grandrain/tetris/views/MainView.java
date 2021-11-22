b40f7d9278f83f01377882c25c899d74d8b2cadf
Max Eve <deadmoz5er@gmail.com>
2021-11-22 12:35:42 +0200

    Use GameController

--- src/main/java/com/grandrain/tetris/views/MainView.java
+++ src/main/java/com/grandrain/tetris/views/MainView.java
@@ -1,5 +1,7 @@
 package com.grandrain.tetris.views;
 
+import com.grandrain.tetris.controllers.GameController;
+import com.grandrain.tetris.controllers.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,17 +9,22 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 510);
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/grandrain/tetris/images/icon.png")));
+        MainController c = fxmlLoader.getController();
+        stage.getIcons().add(new Image(Objects.requireNonNull(
+                getClass().getResourceAsStream("/com/grandrain/tetris/images/icon.png")
+        )));
         stage.setTitle("Тетріс");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+        new GameController(c);
     }
 
     public static void main(String[] args) {
