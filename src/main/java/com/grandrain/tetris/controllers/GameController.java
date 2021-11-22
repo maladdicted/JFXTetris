8b1c2aba2f462649af041524c2496d3a6723062c
Max Eve <deadmoz5er@gmail.com>
2021-11-22 12:31:30 +0200

    Add GameController.java

---
+++ src/main/java/com/grandrain/tetris/controllers/GameController.java
@@ -0,0 +1,69 @@
+package com.grandrain.tetris.controllers;
+
+import com.grandrain.tetris.logic.*;
+import com.grandrain.tetris.logic.events.EventSource;
+import com.grandrain.tetris.logic.events.InputEventListener;
+import com.grandrain.tetris.logic.events.MoveEvent;
+
+public class GameController implements InputEventListener {
+
+    private final Board board = new SimpleBoard(25, 10);
+
+    private final MainController mainController;
+
+    public GameController(MainController c) {
+        mainController = c;
+        board.createNewBrick();
+        mainController.setEventListener(this);
+        mainController.initGameView(board.getBoardMatrix(), board.getViewData());
+        mainController.bindScore(board.getScore().scoreProperty());
+    }
+
+    @Override
+    public DownData onDownEvent(MoveEvent event) {
+        boolean canMove = board.moveBrickDown();
+        ClearRow clearRow = null;
+        if (!canMove) {
+            board.mergeBrickToBackground();
+            clearRow = board.clearRows();
+            if (clearRow.getLinesRemoved() > 0) {
+                board.getScore().add(clearRow.getScoreBonus());
+            }
+            if (board.createNewBrick()) {
+                mainController.gameOver();
+            }
+
+            mainController.refreshGameBackground(board.getBoardMatrix());
+
+        } else {
+            if (event.getEventSource() == EventSource.USER) {
+                board.getScore().add(1);
+            }
+        }
+        return new DownData(clearRow, board.getViewData());
+    }
+
+    @Override
+    public ViewData onLeftEvent(MoveEvent event) {
+        board.moveBrickLeft();
+        return board.getViewData();
+    }
+
+    @Override
+    public ViewData onRightEvent(MoveEvent event) {
+        board.moveBrickRight();
+        return board.getViewData();
+    }
+
+    @Override
+    public ViewData onRotateEvent(MoveEvent event) {
+        board.rotateLeftBrick();
+        return board.getViewData();
+    }
+
+    @Override
+    public void createNewGame() {
+        board.newGame();
+        mainController.refreshGameBackground(board.getBoardMatrix());
+    }
+}
