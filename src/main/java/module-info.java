133ba957db12b2f62f98861953253590aa06bca1
Max Eve <deadmoz5er@gmail.com>
2021-11-29 05:34:08 +0200

    Update exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,15 +2,17 @@ module com.grandrain.tetris {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.media;
     requires java.datatransfer;
     requires java.desktop;
+    requires ini4j;
 
     exports com.grandrain.tetris.logic;
     exports com.grandrain.tetris.logic.events;
     exports com.grandrain.tetris.logic.bricks;
     exports com.grandrain.tetris.logic.rotator;
-    exports com.grandrain.tetris.views;
-    opens com.grandrain.tetris.views to javafx.graphics;
+    exports com.grandrain.tetris;
+    opens com.grandrain.tetris to javafx.graphics;
     exports com.grandrain.tetris.controllers;
     opens com.grandrain.tetris.controllers to javafx.fxml;
     exports com.grandrain.tetris.gui;
