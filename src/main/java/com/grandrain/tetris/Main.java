1916b264c36b4534998339447907c9fcaf3d726a
Max Eve <deadmoz5er@gmail.com>
2021-11-29 09:29:56 +0200

    Update import

--- src/main/java/com/grandrain/tetris/Main.java
+++ src/main/java/com/grandrain/tetris/Main.java
@@ -1,10 +1,12 @@
 package com.grandrain.tetris;
 
+import com.grandrain.tetris.data.Config;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Data.reload();
+        Config.read();
         App.main();
     }
 }
